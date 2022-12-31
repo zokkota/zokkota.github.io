b3f6a322b289d0fd6403b7411f4f4a43b93fd9d6
Théo Franos <68163198+zokkota@users.noreply.github.com>
2022-12-31 18:51:24 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -22,3 +22,37 @@ new Typewriter(txtAnim, {
 .pauseFor(1500)
 .deleteChars(15)
 .start()
+
+
+let slider = document.getElementsByClassName('text');
+
+let etape = 0;
+
+let nbr_slide = slider.length;
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+function enleverActiveText() {
+    for(let i = 0 ; i < nbr_slide ; i++) {
+        slider[i].classList.remove('active');
+    }
+}
+
+suivant.addEventListener('click', function() {
+    etape++;
+    if(etape >= nbr_slide) {
+        etape = 0;
+    }
+    enleverActiveText();
+    slider[etape].classList.add('active');
+})
+
+precedent.addEventListener('click', function() {
+    etape--;
+    if(etape < 0) {
+        etape = nbr_slide - 1;
+    }
+    enleverActiveText();
+    slider[etape].classList.add('active');
+})
